e2f7301169b92ff93727aa656e10727447f9638d
Shubham Chadokar <shubhamchadokar36@gmail.com>
2024-03-27 22:26:50 +0530

    redux modified

---
+++ src/redux/reducers/UserSubscription.js
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const userSubscription = createSlice({
+  name: "usersubscription",
+  initialState: {
+    data: {
+      id: undefined,
+      user: undefined,
+      subscriptions: undefined,
+    },
+  },
+  reducers: {
+    setUserSubscription: (state, action) => {
+      state.data = action.payload;
+      //   console.log("action.payload : ", action.payload);
+      console.log("state.data : ", state.data);
+    },
+  },
+});
+
+export const { setUserSubscription } = userSubscription.actions;
+
+export default userSubscription.reducer;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./reducers/User";
 import countryReducer from "./reducers/CountryReducer";
+import userSubscription from "./reducers/UserSubscription";
 
 const store = configureStore({
   reducer: {
     user: userReducer,
     country: countryReducer,
+    usersubscription: userSubscription,
   },
 });
 
