55f4812813078da1eeb713396e47c9215e71e3ad
harshkhasbage77 <harshkhasbage77@gmail.com>
2024-03-29 20:52:37 +0530

    Feat: Editable Movies Table created for admin

--- src/components/VideoTable.js
+++ src/components/VideoTable.js
@@ -3,6 +3,8 @@ import { AgGridReact } from "ag-grid-react";
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-quartz.css";
 import { movieCertifications } from "../data";
+import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
 
 const CompanyLogoRenderer = ({ value }) => (
   <span
@@ -58,6 +60,7 @@ const CompanyLogoRenderer = ({ value }) => (
 // );
 
 const dateFormatter = (params) => {
+  
   return new Date(params.value).toLocaleDateString("en-us", {
     weekday: "long",
     year: "numeric",
@@ -67,84 +70,139 @@ const dateFormatter = (params) => {
 };
 
 const VideoTable = () => {
-  const [rowData, setRowData] = useState([]);
+  // const [rowData, setRowData] = useState([]);
   const [selectedRows, setSelectedRows] = useState([]);
   const [gridApi, setGridApi] = useState(null);
+
+  const getAllMovies = async () => {
+    console.log("token : ", localStorage.getItem("token"));
+    const response = await axios 
+    .get("/admin/movie",{
+      headers: {Authorization: `Bearer ${localStorage.getItem("token")}`},
+    })
+    .catch((err) =>{
+      console.log(err);
+      throw new Error(err);
+    });
+    console.log('res : ', response);
+    let data = response.data?.data?.movies || [];
+    console.log("hello");
+
+    console.log("data : ", data);
+
+    if (Array.isArray(data) && data.length > 0) {
+      return await data.map((movie) => {
+        return {
+          id: movie?._id,
+          title: movie?.title || "N/A",
+          plot: movie?.plot || "N/A",
+          date: movie?.released || 0,
+          genre: movie?.genres?.join(", ") || "N/A",
+          actors: movie?.cast?.join(", ") || "N/A",
+          writers: movie?.writers?.join(", ") || "N/A",
+          year: movie?.year || 0,
+          IMDB: movie?.imdb?.rating || 0,
+          languages: movie?.languages?.join(", ") || "N/A",
+        };
+      });
+    }
+    console.log("no no no");
+    console.log("data : ", data);
+    console.log("yes yes yes");
+
+    return data;
+  };
+
+  const { data: rowData, error } = useQuery({
+    queryKey: ["all-movies"],
+    queryFn: getAllMovies,
+    refetchOnMount: false,
+    refetchOnWindowFocus: false,
+  });
 
   const colDefs = useMemo(
     () => [
       {
-        field: "video",
+        field: "title",
         width: 340,
         // cellRenderer: VideoImageRenderer,
         checkboxSelection: true,
         headerCheckboxSelection: true,
       },
       {
-        field: "views",
-        width: 130,
-        cellEditor: "agNumberCellEditor",
-        cellEditorParams: {
-          min: 0,
-        },
+        field: "plot",
+        width: 350,
+        cellEditor: "agLargeTextCellEditor",
       },
-      {
-        field: "likes",
-        width: 125,
-        cellEditor: "agNumberCellEditor",
-        cellEditorParams: {
-          min: 0,
-        },
-      },
-      {
-        field: "dislikes",
-        width: 125,
-        cellEditor: "agNumberCellEditor",
-        cellEditorParams: {
-          min: 0,
-        },
-      },
+      // {
+      //   field: "views",
+      //   width: 130,
+      //   cellEditor: "agNumberCellEditor",
+      //   cellEditorParams: {
+      //     min: 0,
+      //   },
+      // },
       {
         field: "date",
         valueFormatter: dateFormatter,
       },
-      {
-        field: "price",
-        width: 130,
-        valueFormatter: (params) => `$${params.value.toLocaleString()}`,
-        cellEditor: "agNumberCellEditor",
-        cellEditorParams: {
-          min: 0,
-        },
-      },
-      {
-        field: "type",
+      // {
+        //   field: "dislikes",
+        //   width: 125,
+        //   cellEditor: "agNumberCellEditor",
+        //   cellEditorParams: {
+          //     min: 0,
+          //   },
+          // },
+          // {
+      //   field: "price",
+      //   width: 130,
+      //   valueFormatter: (params) => `$${params.value.toLocaleString()}`,
+      //   cellEditor: "agNumberCellEditor",
+      //   cellEditorParams: {
+        //     min: 0,
+        //   },
+        // },
+        {
+        field: "genre",
         cellEditor: "agSelectCellEditor",
         cellEditorParams: {
           values: movieCertifications.map((cert) => cert.type),
           valueListGap: 20,
         },
       },
-      { field: "director" },
+      // { field: "director" },
       { field: "actors" },
       { field: "writers" },
       {
-        field: "description",
-        cellEditor: "agLargeTextCellEditor",
-        cellEditorPopup: true,
+        field: "year",
+        width: 125,
+        cellEditor: "agNumberCellEditor",
         cellEditorParams: {
-          maxLength: 100,
+          min: 1850,
         },
       },
+      {
+        field: "IMDB", 
+      },
+      { field: "languages" },
+      // {
+      //   field: "description",
+      //   cellEditor: "agLargeTextCellEditor",
+      //   cellEditorPopup: true,
+      //   cellEditorParams: {
+      //     maxLength: 100,
+      //   },
+      // },
     ],
     []
   );
 
-  useEffect(() => {
-    fetch("https://www.ag-grid.com/example-assets/space-mission-data.json")
-      .then((result) => result.json())
-      .then((rowData) => setRowData(rowData));
-  }, []);
+  // useEffect(() => {
+  //   fetch("https://www.ag-grid.com/example-assets/space-mission-data.json")
+  //     .then((result) => result.json())
+  //     .then((rowData) => setRowData(rowData));
+  // }, []);
 
   const onSelectionChanged = useCallback(() => {
     const selectedNodes = gridApi.getSelectedRows();
