2b42223296c45baacc028c804cf055d7378ec1b0
maverick <manaswiraj195@kgpian.iitkgp.ac.in>
2024-03-31 03:27:38 +0530

    changed the BACKEND URL

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:3002";
+axios.defaults.baseURL = process.env.REACT_APP_BACKEND_URL;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
